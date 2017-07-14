caa587c80370950740e6a573f5f33ca2374f8131
ryota-murakami <dojce1048@gmail.com>
2017-07-14 22:30:16 +0900

    Basic Types :green_apple:

--- src/server.js
+++ src/server.js
@@ -5,22 +5,30 @@ var {buildSchema} = require('graphql')
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `)
 
 // The root provides a resolver function for each API endpoint
 var root = {
-    hello: () => {
-        return 'Hello world!'
-    },
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
+  },
+  random: () => {
+    return Math.random()
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6))
+  },
 }
 
 var app = express()
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
 }))
 app.listen(4000)
 console.log('Running a GraphQL API server at localhost:4000/graphql')

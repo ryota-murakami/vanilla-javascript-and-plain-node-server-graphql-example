54dcfa236cfb30ff0ff1a9424489e8a08ec0a1cc
ryota-murakami <dojce1048@gmail.com>
2017-07-14 21:42:15 +0900

    express server:green_apple:

--- src/server.js
+++ src/server.js
@@ -1,20 +1,26 @@
-import { graphql, buildSchema } from 'graphql'
+var express = require('express')
+var graphqlHTTP = require('express-graphql')
+var {buildSchema} = require('graphql')
 
 // Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
+var schema = buildSchema(`
   type Query {
     hello: String
   }
 `)
 
 // The root provides a resolver function for each API endpoint
-const root = {
+var root = {
     hello: () => {
         return 'Hello world!'
-    }
+    },
 }
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-    console.log(response)
-})
+var app = express()
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}))
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')

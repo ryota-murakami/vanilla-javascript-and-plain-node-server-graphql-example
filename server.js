811ac2603e62726bc6ce78cc4f4da2390ba0b280
ryota-murakami <dojce1048@gmail.com>
2017-07-14 18:20:57 +0900

    Revert "eslint autofix :green_apple:"
    This reverts commit 65435c9214489527a8170c750411d3c617849e9c.

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var { graphql, buildSchema } = require('graphql')
+var { graphql, buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     hello: String
   }
-`)
+`);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-    hello: () => {
-        return 'Hello world!'
-    },
-}
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 // Run the GraphQL query '{ hello }' and print out the response
 graphql(schema, '{ hello }', root).then((response) => {
-    console.log(response)
-})
+  console.log(response);
+});

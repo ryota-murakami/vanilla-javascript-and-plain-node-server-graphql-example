6a2780a2340cdeafffe5e02c9a0aa0a53dd0cd96
ryota-murakami <dojce1048@gmail.com>
2017-07-14 23:26:18 +0900

    Object Types:green_apple:

--- src/server.js
+++ src/server.js
@@ -4,19 +4,40 @@ var {buildSchema} = require('graphql')
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
   }
 `)
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  rollDice: function ({numDice, numSides}) {
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+  constructor (numSides) {
+    this.numSides = numSides
+  }
+
+  rollOnce () {
+    return 1 + Math.floor(Math.random() * this.numSides)
+  }
+
+  roll ({numRolls}) {
     var output = []
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce())
     }
     return output
+  }
+}
+
+// The root provides the top-level API endpoints
+var root = {
+  getDie: function ({numSides}) {
+    return new RandomDie(numSides || 6)
   }
 }
 

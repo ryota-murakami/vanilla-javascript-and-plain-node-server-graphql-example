25070f0e89aa95099efbb1536a3807b94bb7ba37
ryota-murakami <dojce1048@gmail.com>
2017-07-14 19:50:27 +0900

    eslint:green_apple:

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-var { graphql, buildSchema } = require('graphql');
+/* eslint-disable no-console */
+import { graphql, buildSchema } from 'graphql'
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
     hello: String
   }
-`);
+`)
 
 // The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+const root = {
+    hello: () => {
+        return 'Hello world!'
+    }
+}
 
 // Run the GraphQL query '{ hello }' and print out the response
 graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+    console.log(response)
+})

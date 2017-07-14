32f2e89c0600213694e0b719b991b1ec4ad6876c
ryota-murakami <dojce1048@gmail.com>
2017-07-14 22:46:33 +0900

    Passing Arguments :green_apple:

--- src/server.js
+++ src/server.js
@@ -5,23 +5,19 @@ var {buildSchema} = require('graphql')
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `)
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
-  },
-  random: () => {
-    return Math.random()
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6))
-  },
+  rollDice: function ({numDice, numSides}) {
+    var output = []
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+    }
+    return output
+  }
 }
 
 var app = express()

c6368d0667826fba8e0b2d7d72a29cd0130e9e7c
ryota-murakami <dojce1048@gmail.com>
2017-07-16 17:16:00 +0900

    show:green_apple:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 (function () {
   var run_simple_query_btn = document.querySelector('#js-event-listener_run-simple-query-btn');
+  var simple_query_profile = document.querySelector('#js-manipulate-dom_simple-query-profile');
+
+  // toggle
   run_simple_query_btn.addEventListener('click', function () {
-    alert('todo');
+    simple_query_profile.style.display = 'block';
+    requestAnimationFrame(function () { simple_query_profile.style.opacity = 1 });
   })
 })()

f961c058a0b3f4dba371b96c9cf405a5155a2eae
ryota-murakami <dojce1048@gmail.com>
2017-07-16 17:16:45 +0900

    todo:green_apple:

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@
 
   // toggle
   run_simple_query_btn.addEventListener('click', function () {
+    // TODO if display node
     simple_query_profile.style.display = 'block';
     requestAnimationFrame(function () { simple_query_profile.style.opacity = 1 });
+
+    // TODO if display block
   })
 })()

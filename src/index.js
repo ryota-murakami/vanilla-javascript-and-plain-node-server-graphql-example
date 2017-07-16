c108a71f3c0ba955dc9dee37c4f8e7b33fc2e1d8
ryota-murakami <dojce1048@gmail.com>
2017-07-16 17:52:29 +0900

    toggle:green_apple:

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 (function () {
+  // const
+  var CLICK = 'click';
+
+  // dom cache
   var run_simple_query_btn = document.querySelector('#js-event-listener_run-simple-query-btn');
   var simple_query_profile = document.querySelector('#js-manipulate-dom_simple-query-profile');
 
   // toggle
-  run_simple_query_btn.addEventListener('click', function () {
-    // TODO if display node
-    simple_query_profile.style.display = 'block';
-    requestAnimationFrame(function () { simple_query_profile.style.opacity = 1 });
-
-    // TODO if display block
+  run_simple_query_btn.addEventListener(CLICK, function () {
+    if (simple_query_profile.style.opacity == 0) {
+      requestAnimationFrame(function () { simple_query_profile.style.opacity = 1 });
+    } else if (simple_query_profile.style.opacity == 1) {
+      requestAnimationFrame(function () { simple_query_profile.style.opacity = 0 });
+    } else {
+      alert('CSS setting error; at #js-manipulate-dom_simple-query-profile');
+    }
   })
 })()
